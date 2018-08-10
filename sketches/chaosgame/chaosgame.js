19ddb107f04538b11407e784a73a0d9d9327ae32
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2018-08-10 21:02:21 +1000

    new

--- sketches/chaosgame/chaosgame.js
+++ sketches/chaosgame/chaosgame.js
@@ -14,7 +14,7 @@ var s = 0.5;
 var r = 0;
 
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(450, 450);
     background(242, 242, 242);
     textAlign(LEFT,TOP);
     verts = poly(4);
