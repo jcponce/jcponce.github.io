3962cd6beaaa8d9a7d862febbe0aa8e4a51d4bfe
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2018-12-17 18:31:17 +1000

    update

--- sketches/hypoepitrochoid/main.js
+++ sketches/hypoepitrochoid/main.js
@@ -9,27 +9,7 @@
 //The following code is an adaptation found here https://gist.github.com/fogleman/c11a5cbcd845946b851518adedbf6a32
 // written by Fogleman
 
-function hypotrochoid(R, r, d, theta) {
-    var x = (R - r) * cos(theta) + d * cos((R - r) / r * theta);
-    var y = (R - r) * sin(theta) - d * sin((R - r) / r * theta);
-    return createVector(x, y);
-}
-
-function epitrochoid(R, r, d, theta) {
-    var x = (R + r) * cos(theta) - d * cos((R + r) / r * theta);
-    var y = (R + r) * sin(theta) - d * sin((R + r) / r * theta);
-    return createVector(x, y);
-}
-
-function reduceFraction(n, d) {
-    var gcd = function gcd(a, b) {
-        return b ? gcd(b, a % b) : a;
-    };
-    gcd = gcd(n, d);
-    return [n / gcd, d / gcd];
-}
-
-var Controls = function() {
+let Controls = function() {
     this.type = 0;
     this.R = 7;
     this.r = 3;
@@ -38,7 +18,7 @@ var Controls = function() {
     this.scale = 0.6;
 };
 
-var controls = new Controls();
+let controls = new Controls();
 
 let stepTheta;
 let maxTheta;
@@ -48,7 +28,7 @@ function setup() {
     createCanvas(520, 520);
     pixelDensity(1);
     
-    var gui = new dat.GUI({
+    let gui = new dat.GUI({
                           width: 260
                           });
     gui.add(controls, 'type', {
@@ -82,13 +62,11 @@ function draw() {
     let ymin = -h / 2;
     
     
-    var R = controls.R;
-    var r = controls.r;
-    var d = controls.d;
-    var N = reduceFraction(R, r)[1];
-    var rS = controls.scale;
-    
-    
+    let R = controls.R;
+    let r = controls.r;
+    let d = controls.d;
+    let N = reduceFraction(R, r)[1];
+    let rS = controls.scale;
     
     translate(width / 2, height / 2);
     rotate(radians(270 + controls.rotation));
@@ -98,15 +76,15 @@ function draw() {
     
     noFill();
     
-    var func = hypotrochoid;
+    let func = hypotrochoid;
     if (controls.type != 0) {
         func = epitrochoid;
     }
     
-    var lo = createVector(0, 0);
-    var hi = createVector(0, 0);
-    for (var i = 0; i < 360 * N; i++) {
-        var v = func(R, r, d, radians(i));
+    let lo = createVector(0, 0);
+    let hi = createVector(0, 0);
+    for (let i = 0; i < 360 * N; i++) {
+        let v = func(R, r, d, radians(i));
         lo.x = min(lo.x, v.x);
         lo.y = min(lo.y, v.y);
         hi.x = max(hi.x, v.x);
@@ -114,9 +92,9 @@ function draw() {
     }
     //var w = hi.x - lo.x;
     //var h = hi.y - lo.y;
-    var sx = (width * rS) / w;
-    var sy = (height * rS) / h;
-    var s = min(sx, sy);
+    let sx = (width * rS) / w;
+    let sy = (height * rS) / h;
+    let s = min(sx, sy);
     
     if (controls.type != 0) {
         stroke(224, 102, 255);
@@ -126,26 +104,27 @@ function draw() {
     strokeWeight(4);
     strokeJoin(ROUND);
     beginShape();
-    for (var k = 0; k < 360 * N; k++) {
-        var vs = func(R, r, d, radians(k));
+    for (let k = 0; k < 360 * N; k++) {
+        let vs = func(R, r, d, radians(k));
         vertex(vs.x * s, vs.y * s);
     }
     endShape(CLOSE);
     
-    var mP = func(R, r, d, radians(maxTheta));
+    //Drawing Extra circles and points
+    let mP = func(R, r, d, radians(maxTheta));
     
     stroke(255);
     ellipse(0, 0, R * 2 * s); //R circle
     
     noFill();
-    var setCircle;
+    let setCircle;
     if (controls.type != 0) {
         setCircle = r + R;
     } else {
         setCircle = R - r;
     }
-    var xP = s * setCircle * cos(radians(maxTheta));
-    var yP = s * setCircle * sin(radians(maxTheta));
+    let xP = s * setCircle * cos(radians(maxTheta));
+    let yP = s * setCircle * sin(radians(maxTheta));
     stroke(255, 12, 12);
     ellipse(xP, yP, 2 * (r) * s); //r Circle tracing curve
     stroke(26, 255, 255);
@@ -153,10 +132,30 @@ function draw() {
     stroke(120, 22, 220);
     ellipse(xP, yP, 4);//Centre r Circle
     
-    
-    
     fill(255, 209, 26);
     stroke(255, 209, 26)
     ellipse(mP.x * s, mP.y * s, 10);//Point tracing curve
     
 }
+
+function hypotrochoid(R, r, d, theta) {
+    let x = (R - r) * cos(theta) + d * cos((R - r) / r * theta);
+    let y = (R - r) * sin(theta) - d * sin((R - r) / r * theta);
+    return createVector(x, y);
+}
+
+function epitrochoid(R, r, d, theta) {
+    let x = (R + r) * cos(theta) - d * cos((R + r) / r * theta);
+    let y = (R + r) * sin(theta) - d * sin((R + r) / r * theta);
+    return createVector(x, y);
+}
+
+function reduceFraction(n, d) {
+    let gcd = function gcd(a, b) {
+        return b ? gcd(b, a % b) : a;
+    };
+    gcd = gcd(n, d);
+    return [n / gcd, d / gcd];
+}
+
+
