86246be726f5ec529603ec599379d23895b4e4e2
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2019-08-11 10:05:48 +1000

    updated

--- sketches/benhamdisk/main.js
+++ sketches/benhamdisk/main.js
@@ -6,17 +6,11 @@
 
 // Last update ??
 
-let fmC;
-let speed = 100.1;
-
 let slider;
 let t = 0;
+let pw;
 
-
-let ar1;
-let ar2;
-let ar3;
-let ar4;
+let ar1, ar2, ar3, ar4;
 
 function setup() {
     createCanvas(500, 500);
@@ -24,38 +18,37 @@ function setup() {
     pw = width*0.35;
     
     slider = createSlider(-0.7853, 0.7853, 0, 0.0001);
-    slider.style('width:200px')
+    slider.style('width:300px')
     slider.parent('slider');
-    //slider.position(width/2-pw, height-50);
     
-    
-    ar1 = new ArcBenham(0, 0, pw*0.20, 0, 1 * PI / 4);
-    ar2 = new ArcBenham(0, 0, pw*0.38, 1 * PI / 4, 2 * PI / 4);
-    ar3 = new ArcBenham(0, 0, pw*0.56, 2 * PI / 4, 3 * PI / 4);
-    ar4 = new ArcBenham(0, 0, pw*0.74, 3 * PI / 4, 4 * PI / 4);
+    let dif = 0.03;
+    ar1 = new ArcBenham(0, 0, pw*0.20, 0, 1 * PI / 4-dif);
+    ar2 = new ArcBenham(0, 0, pw*(0.20+0.18), 1 * PI / 4+dif, 2 * PI / 4-dif);
+    ar3 = new ArcBenham(0, 0, pw*(0.20+0.18*2), 2 * PI / 4+dif, 3 * PI / 4-dif);
+    ar4 = new ArcBenham(0, 0, pw*(0.20+0.18*3), 3 * PI / 4+dif, 4 * PI / 4-dif);
 }
-
-let pw;
-
-
 
 function draw() {
     background(255);
     
     translate(width / 2, height / 2);
+    
+    //These lines are just for reference
     //fill(0);
     //ellipse(0, 0, 10);
     //ellipse(pw, 0, 10);
     //ellipse(0, -pw, 10);
-    scale(1.2)
+    
+    scale(1.2);
+    
     //circle
     push();
     noFill();
     strokeWeight(1);
     beginShape();
     for (let k = 0; k < 2 * PI; k += 0.01) {
-        let x = pw*0.95*(cos(k) * cos(t) - sin(k) * sin(t));
-        let y = pw*0.95*(cos(k) * sin(t) + sin(k) * cos(t));
+        let x = pw*(0.20+0.18*4)*(cos(k) * cos(t) - sin(k) * sin(t));
+        let y = pw*(0.20+0.18*4)*(cos(k) * sin(t) + sin(k) * cos(t));
         vertex(x, y);
     }
     endShape(CLOSE);
@@ -68,34 +61,31 @@ function draw() {
     strokeWeight(1);
     beginShape();
     for (let k = 0; k < PI; k += 0.01) {
-        let x =pw*0.95*(cos(k) * cos(t) - sin(k) * sin(t));
-        let y = pw*0.95*(cos(k) * sin(t) + sin(k) * cos(t));
+        let x = pw*(0.20+0.18*4)*(cos(k) * cos(t) - sin(k) * sin(t));
+        let y = pw*(0.20+0.18*4)*(cos(k) * sin(t) + sin(k) * cos(t));
         vertex(x, y);
     }
     endShape(CLOSE);
     pop();
     
     
-    //arcs
+    //arcs show and updated
     ar1.show();
     ar2.show();
     ar3.show();
     ar4.show();
     
-    let s = slider.value();//map(constrain(mouseX, 0, width), 0, width, -0.7853, 0.7853);
+    let s = slider.value();
     
     ar1.update(s);
     ar2.update(s);
     ar3.update(s);
     ar4.update(s);
     
-    
-    
+    //rotate circle and semicircle
     t += s;
     
-    //console.log(pw);
 }
-
 
 class ArcBenham {
     constructor(x, y, r, ip, fp) {
@@ -114,7 +104,7 @@ class ArcBenham {
             let p = PI;
             let rw = this.r;
             let sep = 10;
-            strokeWeight(7);
+            strokeWeight(6);
             beginShape();
             for (let k = this.ip; k < this.fp; k += 0.01) {
                 let vx = (rw + i*sep ) * (cos(k) * cos(this.t + p) - sin(k) * sin(this.t + p)) + this.x;
