7aeb31ea8cdd2b93a0567f4aba529badae0309c2
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2020-02-18 10:07:10 +1000

    updated

--- sketches/c-flower/sketch.js
+++ sketches/c-flower/sketch.js
@@ -1,9 +1,8 @@
 /*
+Under Creative Commons License
+https://creativecommons.org/licenses/by-sa/4.0/
 Written by Juan Carlos Ponce Campuzano, 17-Feb-2020
 https://jcponce.github.io/
-
-Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
-https://creativecommons.org/licenses/by-sa/4.0/
 */
 
 let c;
@@ -12,29 +11,25 @@ let ctls = {
   n: 5,
   p: 2,
   q: 1,
-  v: 0.34,
-  r: 0.76,
-  g: Math.random(),
-  b: Math.random(),
-  reset: function() {
-    this.v = Math.random();
-    this.r = Math.random();
-    this.g = Math.random();
-    this.b = Math.random();
-  }
+  trace: true,
+  reset: randomVal
 };
 
 function setup() {
+
   createCanvas(windowWidth, windowHeight);
+  colorMode(RGB, 1, 1, 1, 1);
   pixelDensity(1);
+  background(0);
 
   let gui = new dat.GUI({
-    width: 300
+    width: 290
   });
   gui.add(ctls, 'n', 0, 10, 1).name("n=");
   gui.add(ctls, 'p', 0, 2, 1).name("p=");
   gui.add(ctls, 'q', 0, 2, 1).name("q=");
-  gui.add(ctls, 'reset').name('Change color');
+  gui.add(ctls, 'reset').name("Change color");
+  gui.add(ctls, 'trace').name("Glow");
   gui.close();
 
   c = new polarC();
@@ -42,8 +37,17 @@ function setup() {
 }
 
 function draw() {
-  background(0);
+
+  if (ctls.trace) {
+    background(0, 0, 0, 0.08);
+  } else background(0, 0, 0, 1);
+
   c.plot();
+
+}
+
+function randomVal() {
+  c.randomize();
 }
 
 class polarC {
@@ -52,22 +56,26 @@ class polarC {
     this.d = 2.5;
     this.size = width * 0.055;
     this.angle = 0;
+    this.v = 0.34;
+    this.r = 0.76;
+    this.g = Math.random();
+    this.b = Math.random();
   }
 
   plot() {
     let n = ctls.n;
     translate(width / 2, height / 2);
-    colorMode(RGB, 1, 1, 1);
+
     for (let j = 0; j < 64; j++) {
       let v = [];
       v[j] = j / 64 * (PI / 2 - PI / 56);
-      let c = this.changeColor((j + 1) / 64 - this.angle);
-      if (ctls.v < 0.3) {
-        stroke(c, ctls.g, ctls.blue, c+0.5);
-      } else if (0.3 <= ctls.v && ctls.v < 0.6) {
-        stroke(ctls.r, c, ctls.b, c+0.5);
-      } else if (0.6 <= ctls.v) {
-        stroke(ctls.r, ctls.g, c, c+0.5);
+
+      if (this.v < 0.3) {
+        stroke(this.effects((j) / 64 - this.angle), this.g, this.b, this.effects((j) / 64 - this.angle) + 1 / 2);
+      } else if (0.3 <= this.v && this.v < 0.6) {
+        stroke(this.r, this.effects((j) / 64 - this.angle), this.b, this.effects((j) / 64 - this.angle) + 1 / 2);
+      } else if (0.6 <= this.v) {
+        stroke(this.r, this.g, this.effects((j) / 64 - this.angle), this.effects((j) / 64 - this.angle) + 1 / 2);
       }
 
       noFill();
@@ -75,7 +83,7 @@ class polarC {
       for (let i = 0; i < 360; i = i + 1.5) {
         let nextx, nexty;
         let t = map(i, 0, 360, 0, TWO_PI);
-        nextx = this.size * (this.d * v[j] + pow(cos(n * (t - v[j])), ctls.p) ) * cos(t + this.angle);
+        nextx = this.size * (this.d * v[j] + pow(cos(n * (t - v[j])), ctls.p)) * cos(t + this.angle);
         nexty = this.size * (this.d * v[j] + pow(sin(n * (t - v[j])), ctls.q)) * sin(t + this.angle);
         vertex(nextx, -nexty);
       }
@@ -84,10 +92,10 @@ class polarC {
     }
 
     this.render();
-    
+
   }
 
-  changeColor(x_) {
+  effects(x_) {
     return cos(4 * x_ + 1) / 2;
   }
 
@@ -95,6 +103,12 @@ class polarC {
     this.angle = this.angle + 0.005;
   }
 
+  randomize() {
+    this.v = Math.random();
+    this.r = Math.random();
+    this.g = Math.random();
+    this.b = Math.random();
+  }
 
 }
 
