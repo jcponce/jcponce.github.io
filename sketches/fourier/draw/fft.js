b86fd8b6bbd08145cdc066986ff80d03bccf952f
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2020-06-04 14:15:29 +1000

    ok

--- sketches/fourier/draw/fft.js
+++ sketches/fourier/draw/fft.js
@@ -24,198 +24,198 @@
 (function() {
 	'use strict';
   }());
-
-/* 
- * Computes the discrete Fourier transform (DFT) of the given complex vector, storing the result back into the vector.
- * The vector can have any length. This is a wrapper function.
- */
-function transform(real, imag) {
+  
+  /* 
+   * Computes the discrete Fourier transform (DFT) of the given complex vector, storing the result back into the vector.
+   * The vector can have any length. This is a wrapper function.
+   */
+  function transform(real, imag) {
 	let n = real.length;
 	if (n != imag.length)
-		throw "Mismatched lengths";
+	  throw "Mismatched lengths";
 	if (n == 0)
-		return;
-	else if ((n & (n - 1)) == 0)  // Is power of 2
-		transformRadix2(real, imag);
-	else  // More complicated algorithm for arbitrary sizes
-		transformBluestein(real, imag);
-}
-
-
-/* 
- * Computes the inverse discrete Fourier transform (IDFT) of the given complex vector, storing the result back into the vector.
- * The vector can have any length. This is a wrapper function. This transform does not perform scaling, so the inverse is not a true inverse.
- */
-function inverseTransform(real, imag) {
+	  return;
+	else if ((n & (n - 1)) == 0) // Is power of 2
+	  transformRadix2(real, imag);
+	else // More complicated algorithm for arbitrary sizes
+	  transformBluestein(real, imag);
+  }
+  
+  
+  /* 
+   * Computes the inverse discrete Fourier transform (IDFT) of the given complex vector, storing the result back into the vector.
+   * The vector can have any length. This is a wrapper function. This transform does not perform scaling, so the inverse is not a true inverse.
+   */
+  function inverseTransform(real, imag) {
 	transform(imag, real);
-}
-
-
-/* 
- * Computes the discrete Fourier transform (DFT) of the given complex vector, storing the result back into the vector.
- * The vector's length must be a power of 2. Uses the Cooley-Tukey decimation-in-time radix-2 algorithm.
- */
-function transformRadix2(real, imag) {
+  }
+  
+  
+  /* 
+   * Computes the discrete Fourier transform (DFT) of the given complex vector, storing the result back into the vector.
+   * The vector's length must be a power of 2. Uses the Cooley-Tukey decimation-in-time radix-2 algorithm.
+   */
+  function transformRadix2(real, imag) {
 	// Length letiables
 	let n = real.length;
 	if (n != imag.length)
-		throw "Mismatched lengths";
-	if (n == 1)  // Trivial transform
-		return;
+	  throw "Mismatched lengths";
+	if (n == 1) // Trivial transform
+	  return;
 	let levels = -1;
 	for (let i = 0; i < 32; i++) {
-		if (1 << i == n)
-			levels = i;  // Equal to log2(n)
+	  if (1 << i == n)
+		levels = i; // Equal to log2(n)
 	}
 	if (levels == -1)
-		throw "Length is not a power of 2";
-	
+	  throw "Length is not a power of 2";
+  
 	// Trigonometric tables
 	let cosTable = new Array(n / 2);
 	let sinTable = new Array(n / 2);
 	for (let i = 0; i < n / 2; i++) {
-		cosTable[i] = Math.cos(2 * Math.PI * i / n);
-		sinTable[i] = Math.sin(2 * Math.PI * i / n);
+	  cosTable[i] = Math.cos(2 * Math.PI * i / n);
+	  sinTable[i] = Math.sin(2 * Math.PI * i / n);
 	}
-	
+  
 	// Bit-reversed addressing permutation
 	for (let i = 0; i < n; i++) {
-		let j = reverseBits(i, levels);
-		if (j > i) {
-			let temp = real[i];
-			real[i] = real[j];
-			real[j] = temp;
-			temp = imag[i];
-			imag[i] = imag[j];
-			imag[j] = temp;
-		}
+	  let j = reverseBits(i, levels);
+	  if (j > i) {
+		let temp = real[i];
+		real[i] = real[j];
+		real[j] = temp;
+		temp = imag[i];
+		imag[i] = imag[j];
+		imag[j] = temp;
+	  }
 	}
-	
+  
 	// Cooley-Tukey decimation-in-time radix-2 FFT
 	for (let size = 2; size <= n; size *= 2) {
-		let halfsize = size / 2;
-		let tablestep = n / size;
-		for (let i = 0; i < n; i += size) {
-			for (let j = i, k = 0; j < i + halfsize; j++, k += tablestep) {
-				let l = j + halfsize;
-				let tpre =  real[l] * cosTable[k] + imag[l] * sinTable[k];
-				let tpim = -real[l] * sinTable[k] + imag[l] * cosTable[k];
-				real[l] = real[j] - tpre;
-				imag[l] = imag[j] - tpim;
-				real[j] += tpre;
-				imag[j] += tpim;
-			}
+	  let halfsize = size / 2;
+	  let tablestep = n / size;
+	  for (let i = 0; i < n; i += size) {
+		for (let j = i, k = 0; j < i + halfsize; j++, k += tablestep) {
+		  let l = j + halfsize;
+		  let tpre = real[l] * cosTable[k] + imag[l] * sinTable[k];
+		  let tpim = -real[l] * sinTable[k] + imag[l] * cosTable[k];
+		  real[l] = real[j] - tpre;
+		  imag[l] = imag[j] - tpim;
+		  real[j] += tpre;
+		  imag[j] += tpim;
 		}
+	  }
 	}
-	
+  
 	// Returns the integer whose value is the reverse of the lowest 'width' bits of the integer 'val'.
 	function reverseBits(val, width) {
-		let result = 0;
-		for (let i = 0; i < width; i++) {
-			result = (result << 1) | (val & 1);
-			val >>>= 1;
-		}
-		return result;
+	  let result = 0;
+	  for (let i = 0; i < width; i++) {
+		result = (result << 1) | (val & 1);
+		val >>>= 1;
+	  }
+	  return result;
 	}
-}
-
-
-/* 
- * Computes the discrete Fourier transform (DFT) of the given complex vector, storing the result back into the vector.
- * The vector can have any length. This requires the convolution function, which in turn requires the radix-2 FFT function.
- * Uses Bluestein's chirp z-transform algorithm.
- */
-function transformBluestein(real, imag) {
+  }
+  
+  
+  /* 
+   * Computes the discrete Fourier transform (DFT) of the given complex vector, storing the result back into the vector.
+   * The vector can have any length. This requires the convolution function, which in turn requires the radix-2 FFT function.
+   * Uses Bluestein's chirp z-transform algorithm.
+   */
+  function transformBluestein(real, imag) {
 	// Find a power-of-2 convolution length m such that m >= n * 2 + 1
 	let n = real.length;
 	if (n != imag.length)
-		throw "Mismatched lengths";
+	  throw "Mismatched lengths";
 	let m = 1;
 	while (m < n * 2 + 1)
-		m *= 2;
-	
+	  m *= 2;
+  
 	// Trignometric tables
 	let cosTable = new Array(n);
 	let sinTable = new Array(n);
 	for (let i = 0; i < n; i++) {
-		let j = i * i % (n * 2);  // This is more accurate than j = i * i
-		cosTable[i] = Math.cos(Math.PI * j / n);
-		sinTable[i] = Math.sin(Math.PI * j / n);
+	  let j = i * i % (n * 2); // This is more accurate than j = i * i
+	  cosTable[i] = Math.cos(Math.PI * j / n);
+	  sinTable[i] = Math.sin(Math.PI * j / n);
 	}
-	
+  
 	// Temporary vectors and preprocessing
 	let areal = newArrayOfZeros(m);
 	let aimag = newArrayOfZeros(m);
 	for (let i = 0; i < n; i++) {
-		areal[i] =  real[i] * cosTable[i] + imag[i] * sinTable[i];
-		aimag[i] = -real[i] * sinTable[i] + imag[i] * cosTable[i];
+	  areal[i] = real[i] * cosTable[i] + imag[i] * sinTable[i];
+	  aimag[i] = -real[i] * sinTable[i] + imag[i] * cosTable[i];
 	}
 	let breal = newArrayOfZeros(m);
 	let bimag = newArrayOfZeros(m);
 	breal[0] = cosTable[0];
 	bimag[0] = sinTable[0];
 	for (let i = 1; i < n; i++) {
-		breal[i] = breal[m - i] = cosTable[i];
-		bimag[i] = bimag[m - i] = sinTable[i];
+	  breal[i] = breal[m - i] = cosTable[i];
+	  bimag[i] = bimag[m - i] = sinTable[i];
 	}
-	
+  
 	// Convolution
 	let creal = new Array(m);
 	let cimag = new Array(m);
 	convolveComplex(areal, aimag, breal, bimag, creal, cimag);
-	
+  
 	// Postprocessing
 	for (let i = 0; i < n; i++) {
-		real[i] =  creal[i] * cosTable[i] + cimag[i] * sinTable[i];
-		imag[i] = -creal[i] * sinTable[i] + cimag[i] * cosTable[i];
+	  real[i] = creal[i] * cosTable[i] + cimag[i] * sinTable[i];
+	  imag[i] = -creal[i] * sinTable[i] + cimag[i] * cosTable[i];
 	}
-}
-
-
-/* 
- * Computes the circular convolution of the given real vectors. Each vector's length must be the same.
- */
-function convolveReal(xvec, yvec, outvec) {
+  }
+  
+  
+  /* 
+   * Computes the circular convolution of the given real vectors. Each vector's length must be the same.
+   */
+  function convolveReal(xvec, yvec, outvec) {
 	let n = xvec.length;
 	if (n != yvec.length || n != outvec.length)
-		throw "Mismatched lengths";
+	  throw "Mismatched lengths";
 	convolveComplex(xvec, newArrayOfZeros(n), yvec, newArrayOfZeros(n), outvec, newArrayOfZeros(n));
-}
-
-
-/* 
- * Computes the circular convolution of the given complex vectors. Each vector's length must be the same.
- */
-function convolveComplex(xreal, ximag, yreal, yimag, outreal, outimag) {
+  }
+  
+  
+  /* 
+   * Computes the circular convolution of the given complex vectors. Each vector's length must be the same.
+   */
+  function convolveComplex(xreal, ximag, yreal, yimag, outreal, outimag) {
 	let n = xreal.length;
-	if (n != ximag.length || n != yreal.length || n != yimag.length
-			|| n != outreal.length || n != outimag.length)
-		throw "Mismatched lengths";
-	
+	if (n != ximag.length || n != yreal.length || n != yimag.length ||
+	  n != outreal.length || n != outimag.length)
+	  throw "Mismatched lengths";
+  
 	xreal = xreal.slice();
 	ximag = ximag.slice();
 	yreal = yreal.slice();
 	yimag = yimag.slice();
 	transform(xreal, ximag);
 	transform(yreal, yimag);
-	
+  
 	for (let i = 0; i < n; i++) {
-		let temp = xreal[i] * yreal[i] - ximag[i] * yimag[i];
-		ximag[i] = ximag[i] * yreal[i] + xreal[i] * yimag[i];
-		xreal[i] = temp;
+	  let temp = xreal[i] * yreal[i] - ximag[i] * yimag[i];
+	  ximag[i] = ximag[i] * yreal[i] + xreal[i] * yimag[i];
+	  xreal[i] = temp;
 	}
 	inverseTransform(xreal, ximag);
-	
-	for (let i = 0; i < n; i++) {  // Scaling (because this FFT implementation omits it)
-		outreal[i] = xreal[i] / n;
-		outimag[i] = ximag[i] / n;
+  
+	for (let i = 0; i < n; i++) { // Scaling (because this FFT implementation omits it)
+	  outreal[i] = xreal[i] / n;
+	  outimag[i] = ximag[i] / n;
 	}
-}
-
-
-function newArrayOfZeros(n) {
+  }
+  
+  
+  function newArrayOfZeros(n) {
 	let result = [];
 	for (let i = 0; i < n; i++)
-		result.push(0);
+	  result.push(0);
 	return result;
-}
+  }
--- sketches/fourier/draw/utility.js
+++ sketches/fourier/draw/utility.js
@@ -3,37 +3,37 @@
 var maxLogError = Number.NEGATIVE_INFINITY;
 
 function log10RmsErr(xreal, ximag, yreal, yimag) {
-	const n = xreal.length;
-	if (n != ximag.length || n != yreal.length || n != yimag.length)
-		throw "Mismatched lengths";
-	
-	let err = Math.pow(10, -99 * 2);
-	for (let i = 0; i < n; i++)
-		err += (xreal[i] - yreal[i]) * (xreal[i] - yreal[i]) + (ximag[i] - yimag[i]) * (ximag[i] - yimag[i]);
-	err = Math.sqrt(err / Math.max(n, 1));  // Now this is a root mean square (RMS) error
-	err = Math.log(err) / Math.log(10);
-	maxLogError = Math.max(err, maxLogError);
-	return err;
+  const n = xreal.length;
+  if (n != ximag.length || n != yreal.length || n != yimag.length)
+    throw "Mismatched lengths";
+
+  let err = Math.pow(10, -99 * 2);
+  for (let i = 0; i < n; i++)
+    err += (xreal[i] - yreal[i]) * (xreal[i] - yreal[i]) + (ximag[i] - yimag[i]) * (ximag[i] - yimag[i]);
+  err = Math.sqrt(err / Math.max(n, 1)); // Now this is a root mean square (RMS) error
+  err = Math.log(err) / Math.log(10);
+  maxLogError = Math.max(err, maxLogError);
+  return err;
 }
 
 
 function randomReals(size) {
-	let result = new Array(size);
-	for (let i = 0; i < result.length; i++)
-		result[i] = Math.random() * 2 - 1;
-	return result;
+  let result = new Array(size);
+  for (let i = 0; i < result.length; i++)
+    result[i] = Math.random() * 2 - 1;
+  return result;
 }
 
 function setValuesX(size) {
-	let result = new Array(size);
-	for (var i = 0; i < result.length; i++)
-		result[i] = drawing[i].x;
-	return result;
+  let result = new Array(size);
+  for (var i = 0; i < result.length; i++)
+    result[i] = drawing[i].x;
+  return result;
 }
 
 function setValuesY(size) {
-	let result = new Array(size);
-	for (var i = 0; i < result.length; i++)
-		result[i] = drawing[i].y;
-	return result;
+  let result = new Array(size);
+  for (var i = 0; i < result.length; i++)
+    result[i] = drawing[i].y;
+  return result;
 }
