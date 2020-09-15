d3c9d9b97477c143905597eb5a2181d3f5e60e6c
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2020-09-15 19:23:50 +1000

    updated

--- slides/math3401/mySetup/setup.js
+++ slides/math3401/mySetup/setup.js
@@ -1,11 +1,9 @@
-
 		Reveal.initialize({
 			history: true,
-			transition: 'linear',
 			slideNumber: "c/t",
 			center: true,
 			// Transition style
-  			transition: 'fade', 
+			transition: 'fade',
 
 			math: {
 				//mathjax: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js',
@@ -113,26 +111,29 @@
 			/*loop through a collection of all HTML elements:*/
 			z = document.getElementsByTagName("*");
 			for (i = 0; i < z.length; i++) {
-			  elmnt = z[i];
-			  /*search for elements with a certain atrribute:*/
-			  file = elmnt.getAttribute("include-html");
-			  if (file) {
-				/*make an HTTP request using the attribute value as the file name:*/
-				xhttp = new XMLHttpRequest();
-				xhttp.onreadystatechange = function() {
-				  if (this.readyState == 4) {
-					if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-					if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-					/*remove the attribute, and call this function once more:*/
-					elmnt.removeAttribute("include-html");
-					includeHTML();
-				  }
-				}      
-				xhttp.open("GET", file, true);
-				xhttp.send();
-				/*exit the function:*/
-				return;
-			  }
+				elmnt = z[i];
+				/*search for elements with a certain atrribute:*/
+				file = elmnt.getAttribute("include-html");
+				if (file) {
+					/*make an HTTP request using the attribute value as the file name:*/
+					xhttp = new XMLHttpRequest();
+					xhttp.onreadystatechange = function () {
+						if (this.readyState == 4) {
+							if (this.status == 200) {
+								elmnt.innerHTML = this.responseText;
+							}
+							if (this.status == 404) {
+								elmnt.innerHTML = "Page not found.";
+							}
+							/*remove the attribute, and call this function once more:*/
+							elmnt.removeAttribute("include-html");
+							includeHTML();
+						}
+					}
+					xhttp.open("GET", file, true);
+					xhttp.send();
+					/*exit the function:*/
+					return;
+				}
 			}
-		  };
-	
+		};
