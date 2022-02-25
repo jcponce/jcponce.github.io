66d00b35e9adcc1ade0242c5fc8e4ab01c616b98
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2022-02-25 19:00:48 +1000

    summary

--- math2400/mySetup/setup.js
+++ math2400/mySetup/setup.js
@@ -31,7 +31,9 @@ Reveal.initialize({
 				ra: "\\rightarrow",
 				ds: "\\displaystyle",
 				res: "\\mbox{res}\\,",
-				conj: ["{\\overline{#1}}", 1]
+				conj: ["{\\overline{#1}}", 1],
+				abs: ["{\\left|#1\\right|}", 1],
+				epsilon: "\\varepsilon"
 			}
 		}
 	},
