194198394e1178b7ce3d28078f112db8ef152055
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2022-05-10 14:37:40 +1000

    ok

--- math2400/sketches/surface/fonts.js
+++ math2400/sketches/surface/fonts.js
@@ -1,2 +1,2 @@
 let origin = location.origin.match(/acko\.net/) ? location.origin : 'https://dynamicmath.xyz';
-document.write('<link href="' + origin + '/math2400/sketches/surface/klavika-web.css" rel="stylesheet" type="text/css">');
+document.write('<link href="' + origin + '/math2400/sketches/surface/fonts/KlavikaWeb/klavika-web.css" rel="stylesheet" type="text/css">');
