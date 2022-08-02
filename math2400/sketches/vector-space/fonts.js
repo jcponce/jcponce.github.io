075a4c71aad7681af24e86f26e4a286cb2e71848
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2022-08-02 11:49:12 +1000

    ok

--- math2400/sketches/vector-space/fonts.js
+++
@@ -1,2 +0,0 @@
-let origin = location.origin.match(/acko\.net/) ? location.origin : 'https://acko.net';
-document.write('<link href="' + origin + '/fonts/KlavikaWeb/klavika-web.css" rel="stylesheet" type="text/css">');
