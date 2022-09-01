db13683bf9953e4b718ff68124a5f7ed72f02fc1
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2022-09-02 07:54:34 +1000

    Update sketch.js

--- math2001/sketches/flow2d/sketch.js
+++ math2001/sketches/flow2d/sketch.js
@@ -51,17 +51,17 @@ function setup() {
   }
 
   // Setting up button and sliders
-  buttonTrace = createButton('Trace');
+  buttonTrace = createButton("Trace");
   buttonTrace.position(45, 16);
   buttonTrace.mousePressed(traceShow);
 
   sliderRadius = createSlider(0.01, 200, 70, 0.01);
   sliderRadius.position(buttonTrace.x - 25, buttonTrace.y + 50);
-  sliderRadius.style('width', '100px');
+  sliderRadius.style("width", "100px");
 
   sliderSpeed = createSlider(0.01, 100, 30, 0.01);
   sliderSpeed.position(buttonTrace.x - 25, buttonTrace.y + 100);
-  sliderSpeed.style('width', '100px');
+  sliderSpeed.style("width", "100px");
 
   bs = sliderRadius.value();
 
@@ -76,11 +76,9 @@ function setup() {
   }
 
   //fill(153);
-
 }
 
 function draw() {
-
   cursor(HAND);
 
   translate(width / 2, height / 2);
@@ -100,10 +98,19 @@ function draw() {
     let p = particles[i];
     p.update();
     p.display();
-    if (p.x > width / 2 || p.y > height / 2 || p.x < -width / 2 || p.y < -height / 2 || (pow(p.x + positions[0], 2) + pow(p.y - positions[1], 2)) < sliderRadius.value()) {
+    if (
+      p.x > width / 2 ||
+      p.y > height / 2 ||
+      p.x < -width / 2 ||
+      p.y < -height / 2 ||
+      pow(p.x + positions[0], 2) + pow(p.y - positions[1], 2) <
+        sliderRadius.value()
+    ) {
       particles.splice(i, 1);
       currentParticle--;
-      particles.push(new Particle(width / 2, random(-height / 2, height / 2), t, h));
+      particles.push(
+        new Particle(width / 2, random(-height / 2, height / 2), t, h)
+      );
     }
   }
 
@@ -116,7 +123,12 @@ function draw() {
       noStroke();
       fill(80);
     }
-    ellipse(positions[j * 2], positions[j * 2 + 1], (sliderRadius.value()) * 2, (sliderRadius.value()) * 2);
+    ellipse(
+      positions[j * 2],
+      positions[j * 2 + 1],
+      sliderRadius.value() * 2,
+      sliderRadius.value() * 2
+    );
   }
 
   // Draw UI with button and sliders
@@ -130,16 +142,29 @@ function draw() {
   text("Radius", buttonTrace.x, buttonTrace.y + 45);
   text("Speed", buttonTrace.x, buttonTrace.y + 95);
   //text("Drag circle!", buttonTrace.x, buttonTrace.y + 155);
-
 }
 
-let P = (t, x, y) => -Strength * (sliderSpeed.value() - (sliderSpeed.value() * (sliderRadius.value() * sliderRadius.value()) * (pow(x + positions[0], 2) - pow(y - positions[1], 2))) / ((pow(x + positions[0], 2) + pow(y - positions[1], 2)) * (pow(x + positions[0], 2) + pow(y - positions[1], 2))));
+let P = (t, x, y) =>
+  -Strength *
+  (sliderSpeed.value() -
+    (sliderSpeed.value() *
+      (sliderRadius.value() * sliderRadius.value()) *
+      (pow(x + positions[0], 2) - pow(y - positions[1], 2))) /
+      ((pow(x + positions[0], 2) + pow(y - positions[1], 2)) *
+        (pow(x + positions[0], 2) + pow(y - positions[1], 2))));
 
-let Q = (t, x, y) => -Strength * ((-2 * sliderSpeed.value() * (sliderRadius.value() * sliderRadius.value()) * (x + positions[0]) * (y - positions[1])) / ((pow(x + positions[0], 2) + pow(y - positions[1], 2)) * (pow(x + positions[0], 2) + pow(y - positions[1], 2))));
+let Q = (t, x, y) =>
+  -Strength *
+  ((-2 *
+    sliderSpeed.value() *
+    (sliderRadius.value() * sliderRadius.value()) *
+    (x + positions[0]) *
+    (y - positions[1])) /
+    ((pow(x + positions[0], 2) + pow(y - positions[1], 2)) *
+      (pow(x + positions[0], 2) + pow(y - positions[1], 2))));
 
 //Define particles and how they are moved with Runge–Kutta method of 4th degree.
 class Particle {
-
   constructor(_x, _y, _t, _h) {
     this.x = _x;
     this.y = _y;
@@ -155,14 +180,40 @@ class Particle {
   update() {
     this.k1 = P(this.time, this.x, this.y);
     this.j1 = Q(this.time, this.x, this.y);
-    this.k2 = P(this.time + 1 / 2 * this.h, this.x + 1 / 2 * this.h * this.k1, this.y + 1 / 2 * this.h * this.j1);
-    this.j2 = Q(this.time + 1 / 2 * this.h, this.x + 1 / 2 * this.h * this.k1, this.y + 1 / 2 * this.h * this.j1);
-    this.k3 = P(this.time + 1 / 2 * this.h, this.x + 1 / 2 * this.h * this.k2, this.y + 1 / 2 * this.h * this.j2);
-    this.j3 = Q(this.time + 1 / 2 * this.h, this.x + 1 / 2 * this.h * this.k2, this.y + 1 / 2 * this.h * this.j2);
-    this.k4 = P(this.time + this.h, this.x + this.h * this.k3, this.y + this.h * this.j3);
-    this.j4 = Q(this.time + this.h, this.x + this.h * this.k3, this.y + this.h * this.j3);
-    this.x = this.x + this.h / 6 * (this.k1 + 2 * this.k2 + 2 * this.k3 + this.k4);
-    this.y = this.y + this.h / 6 * (this.j1 + 2 * this.j2 + 2 * this.j3 + this.j4);
+    this.k2 = P(
+      this.time + (1 / 2) * this.h,
+      this.x + (1 / 2) * this.h * this.k1,
+      this.y + (1 / 2) * this.h * this.j1
+    );
+    this.j2 = Q(
+      this.time + (1 / 2) * this.h,
+      this.x + (1 / 2) * this.h * this.k1,
+      this.y + (1 / 2) * this.h * this.j1
+    );
+    this.k3 = P(
+      this.time + (1 / 2) * this.h,
+      this.x + (1 / 2) * this.h * this.k2,
+      this.y + (1 / 2) * this.h * this.j2
+    );
+    this.j3 = Q(
+      this.time + (1 / 2) * this.h,
+      this.x + (1 / 2) * this.h * this.k2,
+      this.y + (1 / 2) * this.h * this.j2
+    );
+    this.k4 = P(
+      this.time + this.h,
+      this.x + this.h * this.k3,
+      this.y + this.h * this.j3
+    );
+    this.j4 = Q(
+      this.time + this.h,
+      this.x + this.h * this.k3,
+      this.y + this.h * this.j3
+    );
+    this.x =
+      this.x + (this.h / 6) * (this.k1 + 2 * this.k2 + 2 * this.k3 + this.k4);
+    this.y =
+      this.y + (this.h / 6) * (this.j1 + 2 * this.j2 + 2 * this.j3 + this.j4);
     this.time += this.h;
   }
 
@@ -173,7 +224,6 @@ class Particle {
     //this.updatey = map(-this.y, -5, 5, -height, height);
     ellipse(-this.x, this.y, 2 * this.radius, 2 * this.radius);
   }
-
 }
 
 let traceShow = (_) => {
@@ -189,7 +239,6 @@ let traceShow = (_) => {
 //    a = 0.02;
 //  }
 //}
-
 
 function mousePressed() {
   checkOver();
@@ -212,15 +261,18 @@ function mouseDragged() {
   }
 
   positions[whichImage * 2] = newx;
-  positions[(whichImage * 2) + 1] = newy;
+  positions[whichImage * 2 + 1] = newy;
 }
 //
 function checkOver() {
   for (let i = 0; i < 1; i++) {
-
-    // Test if the cursor is over the box  
-    if (mouseX - width / 2 > positions[i * 2] - bs && mouseX - width / 2 < positions[i * 2] + bs &&
-      mouseY - height / 2 > positions[i * 2 + 1] - bs && mouseY - height / 2 < positions[i * 2 + 1] + bs) {
+    // Test if the cursor is over the box
+    if (
+      mouseX - width / 2 > positions[i * 2] - bs &&
+      mouseX - width / 2 < positions[i * 2] + bs &&
+      mouseY - height / 2 > positions[i * 2 + 1] - bs &&
+      mouseY - height / 2 < positions[i * 2 + 1] + bs
+    ) {
       //print("mouseover image: "+i);
       whichImage = i;
       bover = true;
