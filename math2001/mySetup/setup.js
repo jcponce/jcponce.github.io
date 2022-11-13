a5672b9dd215ce18ac6cd8cac74f384145c219b0
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2022-11-13 21:04:58 +1000

    ok

--- math2001/mySetup/setup.js
+++ math2001/mySetup/setup.js
@@ -1,4 +1,4 @@
-// Full list of configuration options available here:
+        // Full list of configuration options available here:
 		// https://github.com/hakimel/reveal.js#configuration
 		Reveal.initialize({
 
