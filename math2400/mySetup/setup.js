254aab1ea9b9a7ec5e53232efd0033ca757b8ec6
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2023-05-31 10:03:17 +1000

    ok

--- math2400/mySetup/setup.js
+++ math2400/mySetup/setup.js
@@ -73,6 +73,7 @@
 				boardmarkerWidth: 4,
 				chalkWidth: 3,
 				//storage: "chalkboard-demo",
+				//background: ['rgb(250,250,250)'],
 				grid: { color: 'rgb(50,50,10,0.5)', distance: 80, width: 1},
 				//grid: false,
 				chalkEffect: 0.4,
