5ab586c3987e30e46f0b0ea439e48f93441e4663
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2023-07-20 09:33:29 +1000

    Update sketch.js

--- math1052/sketches/chapter-01/simple-oscillator/sketch.js
+++ math1052/sketches/chapter-01/simple-oscillator/sketch.js
@@ -1,19 +1,18 @@
-let k = 10;
-let m = 30;
-let omega;
-let phi;
-let A = 0;
-let x = 100;
-let y;
-let diam = 64;
+/*
 
-let t = 0;
+ Title: Simulation undamped spring
+ Author: Juan Carlos Ponce Campuzano
+ Date: 18/Jul/2023
+ Instructions: Click on mass and grag to start animation. 
+               Press 'R' to reset.
 
-let wave = [];
-let start = false; // Start animation
+*/
 
-let dragging = false; // Is the object being dragged?
-let rollover = false; // Is the mouse over the ellipse?
+let k = 10; // Spring constant
+let m = 30; // mass
+let x = 100,
+  y; // initial position
+let t = 0; // offset to animate
 
 function setup() {
   createCanvas(750, 400);
@@ -22,47 +21,64 @@ function setup() {
 function draw() {
   background(112, 50, 126);
 
+  // set 'y' coordinate
   y = generalSolution(t) + height / 2;
+
+  drawEqPos();
+  drawSpringCurve(y - 40, 0, 100);
+  updateSpringUser();
+  drawMass();
+  drawSolutionCurve();
 
   if (start) {
     t = t + 0.27;
   }
+}
 
+/* Auxiliar functions */
+
+// General solution in the form A cos(omega t - phi)
+let omega,
+  phi,
+  A = 0;
+function generalSolution(t) {
+  omega = k / m;
+  phi = 0;
+  return A * cos(omega * t + phi);
+}
+
+// Equilibrium position line
+function drawEqPos() {
   push();
   stroke(190, 90);
   line(0, height / 2, width, height / 2);
   pop();
+}
 
-  // Draw spring curve
+// Define spring curve
+function drawSpringCurve(y0, y1, Ap) {
+  //2sin(10 * 2π (t - y_1) / (y_0 - y_1)) + x(A2)
   push();
-  translate(0, 0);
-  springCurve(y - 40, 0, 100);
+  stroke(0);
+  strokeWeight(4);
+  noFill();
+  beginShape();
+  for (let k = -y1; k < y0; k = k + 0.01) {
+    let x = 20 * sin((10 * PI * (k - y1)) / (y0 - y1)) + Ap;
+    let y = k;
+    vertex(x, y);
+  }
+  endShape();
   pop();
+}
 
+let diam = 64;
+function drawMass() {
   push();
   stroke(0);
   strokeWeight(4);
   line(100, y - 40, 100, y - 32);
   pop();
-
-  // Test if mouse if over the circle (mass)
-  let d = dist(x, y, mouseX, mouseY);
-  if (d < diam / 2) {
-    //over = true;
-    rollover = true;
-    cursor("grab");
-  } else {
-    //over = false;
-    rollover = false;
-    cursor("default");
-  }
-  //console.log(rollover);
-
-  // Set and constrain the position of top bar
-  // Adjust location if being dragged
-  if (dragging) {
-    A = mouseY - height / 2;
-  }
 
   push();
   noStroke();
@@ -73,9 +89,10 @@ function draw() {
   }
   circle(x, y, diam);
   pop();
+}
 
-  //if (start) {
-
+let wave = [];
+function drawSolutionCurve() {
   wave.unshift(y);
   translate(100, 0);
   push();
@@ -91,19 +108,58 @@ function draw() {
   if (wave.length > 1000) {
     wave.pop();
   }
-  //}
-
-  changeParameters();
-
-  //console.log(y )
 }
 
-function generalSolution(t) {
-  omega = k / m;
-  phi = 0;
-  return A * cos(omega * t + phi);
+let start = false;
+let dragging = false; // Is the object being dragged?
+let rollover = false; // Is the mouse over the ellipse?
+
+function updateSpringUser() {
+  // Test if mouse if over the circle (mass)
+  let d = dist(x, y, mouseX, mouseY);
+  if (d < diam / 2 && !dragging) {
+    rollover = true;
+    cursor("grab");
+  } else if (d < diam / 2 && dragging) {
+    rollover = true;
+    cursor("grabbing");
+  } else {
+    rollover = false;
+    cursor("default");
+  }
+  //console.log(rollover);
+
+  // Adjust location if being dragged
+  if (dragging) {
+    A = mouseY - height / 2;
+  }
 }
 
+function mousePressed() {
+  if (rollover) {
+    dragging = true;
+    start = false;
+    t = 0;
+  }
+  //console.log(dragging);
+}
+
+function mouseReleased() {
+  dragging = false;
+  start = true;
+  
+  //console.log(dragging);
+}
+
+function keyPressed() {
+  if (keyCode == 82) {
+    t = 0;
+    start = false;
+    A = 0;
+  }
+}
+
+/*
 function changeParameters() {
   if (keyIsDown(UP_ARROW)) {
     A += 2;
@@ -113,7 +169,6 @@ function changeParameters() {
     A -= 2;
     if (A < 0) A = 10;
   }
-  /*
   if (keyIsDown(RIGHT_ARROW)) {
     k += 1;
     if (k > 20) k = 20;
@@ -122,47 +177,7 @@ function changeParameters() {
     k -= 1;
     if (k < 0) k = 2;
   }
-  */
+  
   //console.log(A );
 }
-
-// Define spring curve
-function springCurve(y0, y1, Ap) {
-  //2sin(10 * 2π (t - y_1) / (y_0 - y_1)) + x(A2)
-  stroke(0);
-  strokeWeight(4);
-  noFill();
-  beginShape();
-  for (let k = -y1; k < y0; k = k + 0.01) {
-    let x = 20 * sin((10 * PI * (k - y1)) / (y0 - y1)) + Ap;
-    let y = k;
-    vertex(x, y);
-  }
-  endShape();
-}
-
-function mousePressed() {
-  if (rollover) {
-    dragging = true;
-    start = false;
-    t = 0;
-  }
-
-  //console.log(dragging);
-}
-
-function mouseReleased() {
-  //cursor('default');
-  dragging = false;
-  start = true;
-  //console.log(dragging);
-}
-
-function keyPressed() {
-  //https://www.toptal.com/developers/keycode
-  if (keyCode == 82) {
-    t = 0;
-    start = false;
-    A = 0;
-  }
-}
+*/
