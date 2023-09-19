3534444c948110543267c5923727cbea4527647d
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2023-09-20 09:44:14 +1000

    ok

--- math2001/sketches/terrain-sphere/shaderParkCode.js.js
+++ math2001/sketches/terrain-sphere/shaderParkCode.js.js
@@ -5,11 +5,11 @@
 // not be able to access constants you define outside this function
 // Shader Park will convert this code into a shader for you.
 function shaderParkCode() {
-	let scale= 1.3;
+	let scale = 1.3;
 	let s = getSpace();
 	let n = fractalNoise(s*scale);
 	let grain = noise(s*100)
 	n += grain * .02;
-	color(0, 0, n/5);
+	color(0, 0, n/255*195);
 	sphere(0.7+0.2*n); 
 }
