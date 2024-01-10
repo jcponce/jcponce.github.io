378e8d7bd5bebf2efe64116116925bda7d984740
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2024-01-10 13:21:20 +1000

    new files

--- 2024/template/js/scripts.js
+++ 2024/template/js/scripts.js
@@ -76,7 +76,8 @@ window.onclick = function(event) {
 
 let myInterval = setInterval(function () {
     showMessage ();
-}, 8000);
+}, 30000);
+// 3000 3 seconds
 // 8000 8 seconds
 // 30000 30 seconds
 // 60000 1 min
