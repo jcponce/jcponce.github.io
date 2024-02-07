862ea02fe0a9dfeea0021fb55921f2ee4da225ed
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2024-02-07 19:09:32 +1000

    ok

--- 2024/new/js/scripts.js
+++ 2024/new/js/scripts.js
@@ -92,16 +92,21 @@ function includeHTML() {
 // Button to the top
 // Get the button
 let mybutton = document.getElementById("toTop");
+let prevScrollpos = window.scrollY;
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
+// When the user scrolls down 500px from the top of the document, show the button
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
+  let currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
     mybutton.style.display = "none";
+  } else {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+      mybutton.style.display = "block";
+    }
   }
+  prevScrollpos = currentScrollPos;
 }
 
 // When the user clicks on the button, scroll to the top of the document
