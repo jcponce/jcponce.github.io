4da6ed1622868f4fdcbfa0e00d4d4049c17b2935
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2024-02-07 18:53:23 +1000

    ok

--- 2024/new/js/scripts.js
+++ 2024/new/js/scripts.js
@@ -87,3 +87,25 @@ function includeHTML() {
     }
   }
 };
+
+
+// Button to the top
+// Get the button
+let mybutton = document.getElementById("toTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
