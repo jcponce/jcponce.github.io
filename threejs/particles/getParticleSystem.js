464ed2e92c129c276e71aa5a2b78d0d8a04ff540
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2024-06-06 19:15:00 +1000

    new threejs

---
+++ threejs/particles/getParticleSystem.js
@@ -0,0 +1,231 @@
+import * as THREE from 'three';
+
+const _VS = `
+uniform float pointMultiplier;
+
+attribute float size;
+attribute float angle;
+attribute vec4 aColor;
+
+varying vec4 vColor;
+varying vec2 vAngle;
+
+void main() {
+  vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+
+  gl_Position = projectionMatrix * mvPosition;
+  gl_PointSize = size * pointMultiplier / gl_Position.w;
+
+  vAngle = vec2(cos(angle), sin(angle));
+  vColor = aColor;
+}`;
+
+const _FS = `
+uniform sampler2D diffuseTexture;
+
+varying vec4 vColor;
+varying vec2 vAngle;
+
+void main() {
+  vec2 coords = (gl_PointCoord - 0.5) * mat2(vAngle.x, vAngle.y, -vAngle.y, vAngle.x) + 0.5;
+  gl_FragColor = texture2D(diffuseTexture, coords) * vColor;
+}`;
+
+
+function getLinearSpline(lerp) {
+
+  const points = [];
+  const _lerp = lerp;
+
+  function addPoint(t, d) {
+    points.push([t, d]);
+  }
+
+  function getValueAt(t) {
+    let p1 = 0;
+
+    for (let i = 0; i < points.length; i++) {
+      if (points[i][0] >= t) {
+        break;
+      }
+      p1 = i;
+    }
+
+    const p2 = Math.min(points.length - 1, p1 + 1);
+
+    if (p1 == p2) {
+      return points[p1][1];
+    }
+
+    return _lerp(
+      (t - points[p1][0]) / (
+        points[p2][0] - points[p1][0]),
+      points[p1][1], points[p2][1]);
+  }
+  return { addPoint, getValueAt };
+}
+
+function getParticleSystem(params) {
+  const { camera, emitter, parent, rate, texture } = params;
+  const uniforms = {
+    diffuseTexture: {
+      value: new THREE.TextureLoader().load(texture)
+    },
+    pointMultiplier: {
+      value: window.innerHeight / (2.0 * Math.tan(30.0 * Math.PI / 180.0))
+    }
+  };
+  const _material = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    vertexShader: _VS,
+    fragmentShader: _FS,
+    //blending: THREE.AdditiveBlending,
+    depthTest: true,
+    depthWrite: false,
+    transparent: true,
+    vertexColors: true
+  });
+
+  let _particles = [];
+
+  const geometry = new THREE.BufferGeometry();
+  geometry.setAttribute('position', new THREE.Float32BufferAttribute([], 3));
+  geometry.setAttribute('size', new THREE.Float32BufferAttribute([], 1));
+  geometry.setAttribute('aColor', new THREE.Float32BufferAttribute([], 4));
+  geometry.setAttribute('angle', new THREE.Float32BufferAttribute([], 1));
+
+  const _points = new THREE.Points(geometry, _material);
+
+  parent.add(_points);
+
+  const alphaSpline = getLinearSpline((t, a, b) => {
+    return a + t * (b - a);
+  });
+  alphaSpline.addPoint(0.0, 0.0);
+  alphaSpline.addPoint(0.6, 1.0);
+  alphaSpline.addPoint(1.0, 0.0);
+
+  const colorSpline = getLinearSpline((t, a, b) => {
+    const c = a.clone();
+    return c.lerp(b, t);
+  });
+  colorSpline.addPoint(0.0, new THREE.Color(0xffff00));
+  colorSpline.addPoint(0.0, new THREE.Color(0x00ff00));
+  colorSpline.addPoint(0.0, new THREE.Color(0x00ffff));
+  colorSpline.addPoint(1.0, new THREE.Color(0xffffff));
+
+  const sizeSpline = getLinearSpline((t, a, b) => {
+    return a + t * (b - a);
+  });
+  sizeSpline.addPoint(0.0, 0.0);
+  sizeSpline.addPoint(1.0, 1.0);
+ // max point size = 512; => console.log(ctx.getParameter(ctx.ALIASED_POINT_SIZE_RANGE));
+  const maxVelocity = 1.5;
+  const radius = 0.5;
+  const maxLife = 9.5;
+  const maxSize = 0.15;
+  let gdfsghk = 0.0;
+  function _AddParticles(timeElapsed) {
+    gdfsghk += timeElapsed;
+    const n = Math.floor(gdfsghk * rate);
+    gdfsghk -= n / rate;
+    for (let i = 0; i < n; i += 1) {
+      const life = (Math.random() * 0.75 + 0.25) * maxLife;
+      _particles.push({
+        position: new THREE.Vector3(
+          (Math.random() * 2 - 1) * radius,
+          (Math.random() * 2 - 1) * radius,
+          (Math.random() * 2 - 1) * radius).add(emitter.position),
+        size: (Math.random() * 0.5 + 0.5) * maxSize,
+        colour: new THREE.Color(),
+        alpha: 1.0,
+        life: life,
+        maxLife: life,
+        rotation: Math.random() * 2.0 * Math.PI,
+        rotationRate: Math.random() * 0.01 - 0.005,
+        velocity: new THREE.Vector3(
+          (Math.random() * 2 - 1) * radius * maxVelocity,
+          (Math.random() * 2 - 1) * radius * maxVelocity,
+          (Math.random() * 2 - 1) * radius * maxVelocity),
+      });
+    }
+  }
+
+  function _UpdateGeometry() {
+    const positions = [];
+    const sizes = [];
+    const colours = [];
+    const angles = [];
+
+    for (let p of _particles) {
+      positions.push(p.position.x, p.position.y, p.position.z);
+      colours.push(p.colour.r, p.colour.g, p.colour.b, p.alpha);
+      sizes.push(p.currentSize);
+      angles.push(p.rotation);
+    }
+
+    geometry.setAttribute(
+      'position', new THREE.Float32BufferAttribute(positions, 3));
+    geometry.setAttribute(
+      'size', new THREE.Float32BufferAttribute(sizes, 1));
+    geometry.setAttribute(
+      'aColor', new THREE.Float32BufferAttribute(colours, 4));
+    geometry.setAttribute(
+      'angle', new THREE.Float32BufferAttribute(angles, 1));
+
+    geometry.attributes.position.needsUpdate = true;
+    geometry.attributes.size.needsUpdate = true;
+    geometry.attributes.aColor.needsUpdate = true;
+    geometry.attributes.angle.needsUpdate = true;
+  }
+  _UpdateGeometry();
+
+  function _UpdateParticles(timeElapsed) {
+    for (let p of _particles) {
+      p.life -= timeElapsed;
+    }
+
+    _particles = _particles.filter(p => {
+      return p.life > 0.0;
+    });
+
+    for (let p of _particles) {
+      const t = 1.0 - p.life / p.maxLife;
+      p.rotation += p.rotationRate;
+      p.alpha = alphaSpline.getValueAt(t);
+      p.currentSize = p.size * sizeSpline.getValueAt(t);
+      p.colour.copy(colorSpline.getValueAt(t));
+
+      p.position.add(p.velocity.clone().multiplyScalar(timeElapsed));
+
+      const drag = p.velocity.clone();
+      drag.multiplyScalar(timeElapsed * 0.1);
+      drag.x = Math.sign(p.velocity.x) * Math.min(Math.abs(drag.x), Math.abs(p.velocity.x));
+      drag.y = Math.sign(p.velocity.y) * Math.min(Math.abs(drag.y), Math.abs(p.velocity.y));
+      drag.z = Math.sign(p.velocity.z) * Math.min(Math.abs(drag.z), Math.abs(p.velocity.z));
+      p.velocity.sub(drag);
+    }
+
+    _particles.sort((a, b) => {
+      const d1 = camera.position.distanceTo(a.position);
+      const d2 = camera.position.distanceTo(b.position);
+
+      if (d1 > d2) {
+        return -1;
+      }
+      if (d1 < d2) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+
+  function update(timeElapsed) {
+    _AddParticles(timeElapsed);
+    _UpdateParticles(timeElapsed);
+    _UpdateGeometry();
+  }
+  return { update };
+}
+
+export { getParticleSystem };
---
+++ threejs/particles/index.js
@@ -0,0 +1,93 @@
+import * as THREE from "three";
+import getLayer from "./getLayer.js";
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+import { getParticleSystem } from "./getParticleSystem.js";
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w, h);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
+
+/*
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshStandardMaterial({
+  color: 0xffff00,
+});
+const cube = new THREE.Mesh(geometry, material);
+cube.position.y = -1;
+scene.add(cube);
+*/
+
+const ambientLight = new THREE.AmbientLight(0x000000);
+scene.add(ambientLight);
+
+const light1 = new THREE.DirectionalLight(0xffffff, 3);
+light1.position.set(0, 200, 0);
+scene.add(light1);
+
+const light2 = new THREE.DirectionalLight(0xffffff, 3);
+light2.position.set(100, 200, 100);
+scene.add(light2);
+
+const light3 = new THREE.DirectionalLight(0xffffff, 3);
+light3.position.set(- 100, - 200, - 100);
+scene.add(light3);
+
+const geometry = new THREE.TorusKnotGeometry(0.6, 0.15, 100, 16);
+//const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } ); 
+const material = new THREE.MeshLambertMaterial({ color: 0x049ef4, emissive: 0x000000 });
+
+const torusKnot = new THREE.Mesh(geometry, material); scene.add(torusKnot);
+scene.add(torusKnot);
+
+
+const fireEffect = getParticleSystem({
+  camera,
+  emitter: torusKnot,
+  parent: scene,
+  rate: 50.0,
+  texture: 'img/circle.png',
+});
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+scene.add(hemiLight);
+
+// Sprites BG
+const gradientBackground = getLayer({
+  hue: 0.6,
+  numSprites: 8,
+  opacity: 0.2,
+  radius: 10,
+  size: 24,
+  z: -10.5,
+});
+scene.add(gradientBackground);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torusKnot.rotation.x += 0.005;
+  torusKnot.rotation.y += 0.007;
+  fireEffect.update(0.016);
+  renderer.render(scene, camera);
+  controls.update();
+}
+
+animate();
+
+function handleWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', handleWindowResize, false);
