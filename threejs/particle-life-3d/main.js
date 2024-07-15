9b6fb55667a43415092f876393e0bbc09621f4ee
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2024-07-16 09:15:39 +1000

    ok

--- threejs/particle-life-3d/main.js
+++ threejs/particle-life-3d/main.js
@@ -20,6 +20,203 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 
 ///*
+let scene, camera, renderer, controls;
+let particles = [];
+let numParticles = 1200; // Increase number of particles
+let numTypes;
+let colorStep;
+let forces, minDistances, radii;
+
+let worldDimensions = new THREE.Vector3(500, 500, 500);
+
+class Particle {
+    constructor() {
+
+        this.initialize();
+        let hue = this.type * colorStep;
+        let color = new THREE.Color(`hsl(${hue}, 100%, 50%)`);
+
+        let geometry = new THREE.SphereGeometry(2, 32, 32);
+        let material = new THREE.MeshLambertMaterial({
+            color: color,
+            emissive: 0x000000,
+        });
+
+        this.mesh = new THREE.Mesh(geometry, material);
+        this.mesh.position.copy(this.position);
+        scene.add(this.mesh);
+    }
+
+    initialize() {
+        let rad = Math.random() * 100;
+        let angX = Math.random() * Math.PI * 2;
+        let angY = Math.random() * Math.PI * 2;
+        this.position = new THREE.Vector3(rad * Math.cos(angX), rad * Math.sin(angX), rad * Math.sin(angY));
+        this.velocity = new THREE.Vector3(0, 0, 0);
+        this.type = Math.floor(Math.random() * numTypes);
+    }
+
+    update() {
+        let totalForce = new THREE.Vector3();
+        for (let i = 0; i < particles.length; i++) {
+            if (particles[i] !== this) {
+                let direction = particles[i].position.clone().sub(this.position);
+
+                if (direction.x > worldDimensions.x / 2) direction.x -= worldDimensions.x;
+                if (direction.x < -worldDimensions.x / 2) direction.x += worldDimensions.x;
+                if (direction.y > worldDimensions.y / 2) direction.y -= worldDimensions.y;
+                if (direction.y < -worldDimensions.y / 2) direction.y += worldDimensions.y;
+                if (direction.z > worldDimensions.z / 2) direction.z -= worldDimensions.z;
+                if (direction.z < -worldDimensions.z / 2) direction.z += worldDimensions.z;
+
+
+                let dis = direction.length();
+                direction.normalize();
+
+
+                if (dis < minDistances[this.type][particles[i].type]) {
+                    let force = direction.clone().multiplyScalar(Math.abs(forces[this.type][particles[i].type]) * -3 * ((minDistances[this.type][particles[i].type] - dis) / minDistances[this.type][particles[i].type]));
+                    totalForce.add(force);
+                }
+
+                if (dis < radii[this.type][particles[i].type]) {
+                    let force = direction.clone().multiplyScalar(forces[this.type][particles[i].type] * ((radii[this.type][particles[i].type] - dis) / radii[this.type][particles[i].type]));
+                    totalForce.add(force);
+                }
+            }
+        }
+
+        this.velocity.add(totalForce.multiplyScalar(0.05));
+        this.position.add(this.velocity);
+        this.velocity.multiplyScalar(0.85);
+
+        if (this.position.x > worldDimensions.x / 2) this.position.x -= worldDimensions.x;
+        if (this.position.x < -worldDimensions.x / 2) this.position.x += worldDimensions.x;
+        if (this.position.y > worldDimensions.y / 2) this.position.y -= worldDimensions.y;
+        if (this.position.y < -worldDimensions.y / 2) this.position.y += worldDimensions.y;
+        if (this.position.z > worldDimensions.z / 2) this.position.z -= worldDimensions.z;
+        if (this.position.z < -worldDimensions.z / 2) this.position.z += worldDimensions.z;
+
+        this.mesh.position.copy(this.position);
+    }
+}
+
+
+init();
+animate();
+
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+    camera.position.z = 300;
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    document.body.appendChild(renderer.domElement);
+
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.05;
+    //controls.screenSpacePanning = false;
+    //controls.minDistance = 500;
+    //controls.maxDistance = 3000;
+
+    let directionalLight = new THREE.DirectionalLight(0xffffff, 4);
+    directionalLight.position.set(1, 1, 1).normalize();
+    scene.add(directionalLight);
+
+    const ambientLight = new THREE.AmbientLight(0x000000);
+    scene.add(ambientLight);
+
+    const light1 = new THREE.DirectionalLight(0xffffff, 3);
+    light1.position.set(0, 200, 0);
+    scene.add(light1);
+
+    const light2 = new THREE.DirectionalLight(0xffffff, 3);
+    light2.position.set(100, 200, 100);
+    scene.add(light2);
+
+    const light3 = new THREE.DirectionalLight(0xffffff, 3);
+    light3.position.set(- 100, - 200, - 100);
+    scene.add(light3);
+
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+    scene.add(hemiLight);
+
+    // Sprites BackGround
+    const gradientBackground = getLayer({
+        hue: 0.6,
+        numSprites: 8,
+        opacity: 0.2,
+        radius: 500,
+        size: 1000,
+        z: -400,
+    });
+    scene.add(gradientBackground);
+
+
+    numTypes = Math.floor(Math.random() * 5) + 2;
+    colorStep = 360 / numTypes;
+
+    forces = Array.from({ length: numTypes }, () => Array(numTypes).fill(0));
+    minDistances = Array.from({ length: numTypes }, () => Array(numTypes).fill(0));
+    radii = Array.from({ length: numTypes }, () => Array(numTypes).fill(0));
+    setParameters();
+
+    for (let i = 0; i < numParticles; i++) {
+        particles.push(new Particle());
+    }
+
+    window.addEventListener('resize', onWindowResize, false);
+    document.addEventListener('dblclick', setParameters, false);
+    document.getElementById('resetButton').addEventListener('click', resetParticles, false);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    updateParticles();
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function setParameters() {
+
+    for (let i = 0; i < numTypes; i++) {
+        for (let j = 0; j < numTypes; j++) {
+            forces[i][j] = Math.random() * 0.7 + 0.3;
+            if (Math.random() < 0.5) forces[i][j] *= -1;
+            minDistances[i][j] = Math.random() * 20 + 30;
+            radii[i][j] = Math.random() * 180 + 70;
+        }
+    }
+
+}
+
+function updateParticles() {
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].update();
+    }
+}
+
+
+function resetParticles() {
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].initialize();
+        particles[i].mesh.position.copy(particles[i].position);
+    }
+}
+//*/
+
+
+/*
 let scene, camera, renderer, particles;
 let numParticles = 1200;
 let numTypes;
@@ -245,208 +442,9 @@ function updateParticles() {
 
 init();
 animate();
-//*/
-
-/*
-import * as THREE from "three";
-import getLayer from "./getLayer.js";
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-
-let scene, camera, renderer, controls;
-let particles = [];
-let numParticles = 1500; // Increase number of particles
-let numTypes;
-let colorStep;
-let forces, minDistances, radii;
-
-let worldDimensions = new THREE.Vector3(500, 500, 500);
-
-class Particle {
-    constructor() {
-
-        this.initialize();
-        let hue = this.type * colorStep;
-        let color = new THREE.Color(`hsl(${hue}, 100%, 50%)`);
-
-        let geometry = new THREE.SphereGeometry(2, 32, 32);
-        let material = new THREE.MeshLambertMaterial({
-            color: color,
-            emissive: 0x000000,
-        });
-
-        this.mesh = new THREE.Mesh(geometry, material);
-        this.mesh.position.copy(this.position);
-        scene.add(this.mesh);
-    }
-
-    initialize() {
-        let rad = Math.random() * 100;
-        let angX = Math.random() * Math.PI * 2;
-        let angY = Math.random() * Math.PI * 2;
-        this.position = new THREE.Vector3(rad * Math.cos(angX), rad * Math.sin(angX), rad * Math.sin(angY));
-        this.velocity = new THREE.Vector3(0, 0, 0);
-        this.type = Math.floor(Math.random() * numTypes);
-    }
-
-    update() {
-        let totalForce = new THREE.Vector3();
-        for (let i = 0; i < particles.length; i++) {
-            if (particles[i] !== this) {
-                let direction = particles[i].position.clone().sub(this.position);
-
-                if (direction.x > worldDimensions.x / 2) direction.x -= worldDimensions.x;
-                if (direction.x < -worldDimensions.x / 2) direction.x += worldDimensions.x;
-                if (direction.y > worldDimensions.y / 2) direction.y -= worldDimensions.y;
-                if (direction.y < -worldDimensions.y / 2) direction.y += worldDimensions.y;
-                if (direction.z > worldDimensions.z / 2) direction.z -= worldDimensions.z;
-                if (direction.z < -worldDimensions.z / 2) direction.z += worldDimensions.z;
-
-
-                let dis = direction.length();
-                direction.normalize();
-
-
-                if (dis < minDistances[this.type][particles[i].type]) {
-                    let force = direction.clone().multiplyScalar(Math.abs(forces[this.type][particles[i].type]) * -3 * ((minDistances[this.type][particles[i].type] - dis) / minDistances[this.type][particles[i].type]));
-                    totalForce.add(force);
-                }
-
-                if (dis < radii[this.type][particles[i].type]) {
-                    let force = direction.clone().multiplyScalar(forces[this.type][particles[i].type] * ((radii[this.type][particles[i].type] - dis) / radii[this.type][particles[i].type]));
-                    totalForce.add(force);
-                }
-            }
-        }
-
-        this.velocity.add(totalForce.multiplyScalar(0.05));
-        this.position.add(this.velocity);
-        this.velocity.multiplyScalar(0.85);
-
-        if (this.position.x > worldDimensions.x / 2) this.position.x -= worldDimensions.x;
-        if (this.position.x < -worldDimensions.x / 2) this.position.x += worldDimensions.x;
-        if (this.position.y > worldDimensions.y / 2) this.position.y -= worldDimensions.y;
-        if (this.position.y < -worldDimensions.y / 2) this.position.y += worldDimensions.y;
-        if (this.position.z > worldDimensions.z / 2) this.position.z -= worldDimensions.z;
-        if (this.position.z < -worldDimensions.z / 2) this.position.z += worldDimensions.z;
-
-        this.mesh.position.copy(this.position);
-    }
-}
-
-
-init();
-animate();
-
-function init() {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
-    camera.position.z = 300;
-
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.toneMapping = THREE.ACESFilmicToneMapping;
-    document.body.appendChild(renderer.domElement);
-
-    controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.dampingFactor = 0.05;
-    //controls.screenSpacePanning = false;
-    //controls.minDistance = 500;
-    //controls.maxDistance = 3000;
-
-    let directionalLight = new THREE.DirectionalLight(0xffffff, 4);
-    directionalLight.position.set(1, 1, 1).normalize();
-    scene.add(directionalLight);
-
-    const ambientLight = new THREE.AmbientLight(0x000000);
-    scene.add(ambientLight);
-
-    const light1 = new THREE.DirectionalLight(0xffffff, 3);
-    light1.position.set(0, 200, 0);
-    scene.add(light1);
-
-    const light2 = new THREE.DirectionalLight(0xffffff, 3);
-    light2.position.set(100, 200, 100);
-    scene.add(light2);
-
-    const light3 = new THREE.DirectionalLight(0xffffff, 3);
-    light3.position.set(- 100, - 200, - 100);
-    scene.add(light3);
-
-    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
-    scene.add(hemiLight);
-
-    // Sprites BackGround
-    const gradientBackground = getLayer({
-        hue: 0.6,
-        numSprites: 8,
-        opacity: 0.2,
-        radius: 500,
-        size: 1000,
-        z: -400,
-    });
-    scene.add(gradientBackground);
-
-
-    numTypes = Math.floor(Math.random() * 5) + 2;
-    colorStep = 360 / numTypes;
-
-    forces = Array.from({ length: numTypes }, () => Array(numTypes).fill(0));
-    minDistances = Array.from({ length: numTypes }, () => Array(numTypes).fill(0));
-    radii = Array.from({ length: numTypes }, () => Array(numTypes).fill(0));
-    setParameters();
-
-    for (let i = 0; i < numParticles; i++) {
-        particles.push(new Particle());
-    }
-
-    window.addEventListener('resize', onWindowResize, false);
-    document.addEventListener('dblclick', setParameters, false);
-    document.getElementById('resetButton').addEventListener('click', resetParticles, false);
-}
-
-function animate() {
-    requestAnimationFrame(animate);
-    updateParticles();
-    controls.update();
-    renderer.render(scene, camera);
-}
-
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-function setParameters() {
-
-    for (let i = 0; i < numTypes; i++) {
-        for (let j = 0; j < numTypes; j++) {
-            forces[i][j] = Math.random() * 0.7 + 0.3;
-            if (Math.random() < 0.5) forces[i][j] *= -1;
-            minDistances[i][j] = Math.random() * 20 + 30;
-            radii[i][j] = Math.random() * 180 + 70;
-        }
-    }
-
-}
-
-function updateParticles() {
-    for (let i = 0; i < particles.length; i++) {
-        particles[i].update();
-    }
-}
-
-
-function resetParticles() {
-    for (let i = 0; i < particles.length; i++) {
-        particles[i].initialize();
-        particles[i].mesh.position.copy(particles[i].position);
-    }
-}
 */
+
+
 
 
 
