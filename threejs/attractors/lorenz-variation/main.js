5f0ebaebc2e2950a892f6f84e1995900f54d9945
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2024-08-01 19:01:46 +1000

    ok

--- threejs/attractors/lorenz-variation/main.js
+++ threejs/attractors/lorenz-variation/main.js
@@ -109,6 +109,28 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 //scene.add(axesHelper);
 
 /**
+ * Lorenz Attractor variation
+ */
+const initialParams = {
+    Attractor: "Lorenz variation",
+    sigma: 10,
+    rho: 28,
+    beta: 8 / 3,
+};
+
+const params = { ...initialParams };
+
+let attractor = (x, y, z) => {
+    const { sigma, rho, beta } = params;
+ 
+    const dx = (sigma * (y - x) + (sin(y / 5) * sin(z / 5) * 200)) * .65;
+    const dy = (x * (rho - z) - y + (sin(x / 5) * sin(z / 5) * 200)) * .65;
+    const dz = (x * y - beta * z + cos(y / 5) * cos(x / 5) * 200) * .65;
+
+    return { dx, dy, dz };
+};
+
+/**
  * Reset functions
  */
 const resetInitialPositions = () => {
