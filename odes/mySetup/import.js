da2011750b4859d01dc4b8e96be94c46d412af64
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2025-06-02 14:41:03 +1000

    removed files

--- odes/mySetup/import.js
+++
@@ -1,8 +0,0 @@
-import 'revealjs/dist/reveal.js'
-import 'revealjs/plugin/math/math.js'
-import 'revealjs/plugin/highlight/highlight.js'
-import 'revealjs/plugin/zoom/zoom.js'
-import 'revealjs/plugin/search/search.js'
-import 'revealjs/third-party-plugin/chalkboard/plugin.js'
-import 'revealjs/third-party-plugin/customcontrols/plugin.js'
-import 'revealjs/third-party-plugin/menu/menu.js'
