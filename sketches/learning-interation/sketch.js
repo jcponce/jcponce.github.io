f9b7451ca6af14c084a83aa949133e66d0408486
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2025-11-21 20:29:12 +1000

    Update sketch.js

--- sketches/learning-interation/sketch.js
+++ sketches/learning-interation/sketch.js
@@ -13,8 +13,8 @@ function setup() {
   colorMode(HSB, 360, 100, 100);
 
   // Number of particles per system
-  const numStaff = 100;
-  const numStudents = 250;
+  const numStaff = 40;
+  const numStudents = 200;
 
   // Left spawn region (30% width)
   for (let i = 0; i < numStaff; i++) {
