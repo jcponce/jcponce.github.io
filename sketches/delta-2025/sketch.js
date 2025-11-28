19b1eb5189bcfc4fb3c8e7f872a7a6db36871eac
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2025-11-28 17:47:00 +1000

    Update sketch.js

--- sketches/delta-2025/sketch.js
+++ sketches/delta-2025/sketch.js
@@ -50,6 +50,7 @@ async function setup() {
 
   bgImg = await loadImage('delta-uq.jpg');
   fgImg = await loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Grouse_Mountain_%288598307419%29.jpg/960px-Grouse_Mountain_%288598307419%29.jpg?20210319145510');
+  //fgImg = await loadImage('https://upload.wikimedia.org/wikipedia/commons/4/41/Exotic_Rainforest_%285141378112%29.jpg');
   paperImg = await loadImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Brown_paper_bag_texture.jpg/640px-Brown_paper_bag_texture.jpg');
 
   textAlign(CENTER, CENTER);
